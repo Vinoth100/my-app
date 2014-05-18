07b99f37cd71ba5b2fab1a253d0a3a40a2838e18
Vinoth100 <vinothkumar.dharmalingam@gmail.com>
2014-05-18 16:19:10 -0700

    First commit

---
+++ src/test/java/com/example/my_app/AppTest.java
@@ -0,0 +1,19 @@
+package com.example.my_app;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMultiply() {
+		fail("Not yet implemented");
+	}
+
+}
