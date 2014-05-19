6452749b76e9ae7d1798f2f70fef91fe461cb647
Vinoth100 <vinothkumar.dharmalingam@gmail.com>
2014-05-18 18:28:56 -0700

    added one method

--- src/main/java/com/example/my_app/App.java
+++ src/main/java/com/example/my_app/App.java
@@ -15,6 +15,11 @@ public class App
 		return a*b;
 	}
 	
+	public int subtract(int a, int b){
+		return a-b;
+	}
+	
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
